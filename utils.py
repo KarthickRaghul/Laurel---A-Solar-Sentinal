25987b78c88a03f3de36f80bcc848b951041ce1d
Karthick Raghul S J <karthickraghul123@gmail.com>
2025-09-03 00:12:41 +0530

    base code for creation

---
+++ utils.py
@@ -0,0 +1,10 @@
+# scanner/utils.py
+import json
+
+def save_json(data, file_path):
+    with open(file_path, "w") as f:
+        json.dump(data, f, indent=2)
+
+def load_json(file_path):
+    with open(file_path, "r") as f:
+        return json.load(f)
